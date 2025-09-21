9ee03147d80049e473c39dbc8b1c3aa3b429cdc5
EdwardQuach <vkspecialed@gmail.com>
2025-09-21 09:25:57 -0700

    split added

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if shot.collides_with(asteroid):
-                    asteroid.kill()
                     shot.kill()
+                    asteroid.split()
         
         for obj in drawable:
             obj.draw(screen)
